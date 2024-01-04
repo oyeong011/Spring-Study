a29f4e385ec8478e124f80ffd9176e117366e184
oyeong011 <oyeong011@gmail.com>
2024-01-04 12:57:24 +0900

    Study : exception

--- mvc2/login-start/src/main/java/hello/login/WebConfig.java
+++ mvc2/login-start/src/main/java/hello/login/WebConfig.java
@@ -1,12 +1,38 @@
 package hello.login;
 
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
+import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver()); // 로그인 멤버 리졸버 등록
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1) // 필터 순서
+                .addPathPatterns("/**") // 모든 요청에 인터셉터 적용
+                .excludePathPatterns("/css/**","/*.ico","/error"); // 적용 제외 경로
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2) // 필터 순서
+                .addPathPatterns("/**") // 모든 요청에 인터셉터 적용
+                .excludePathPatterns("/","/members/add","/login","/logout","/css/**","/*.ico","/error"); // 적용 제외 경로
+    }
+
 
     @Bean
     public FilterRegistrationBean logFilter(){
--- mvc2/login-start/src/main/java/hello/login/web/interceptor/LogInterceptor.java
+++ mvc2/login-start/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -1,6 +1,7 @@
 package hello.login.web.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,22 +10,39 @@ import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         String requestURI = request.getRequestURI();
         String uuid = (String) request.getAttribute("uuid");
 
-        request.setAttribute("logId", uuid);
+        request.setAttribute(LOG_ID, uuid);
+
+        if(handler instanceof HandlerMethod){
+            HandlerMethod hm = (HandlerMethod) handler; //호출할 컨트롤러 메서드의 모든 정보가 포함되어 있음
+        }
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+
+        return true;
+
 
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        request.getAttribute("logId");
+        Object requestURI = request.getAttribute(LOG_ID);
+        String logId = (String) request.getAttribute(LOG_ID);
+        log.info("RESPONSE [{}][{}][{}]", logId, requestURI, handler);
+        if(ex != null){
+            log.error("afterCompletion error!!", ex);
+        }
     }
 }
